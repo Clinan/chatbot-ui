5f0b3b564af549b294c6a3882e5561f40cc9472b
xli <xli3@leqee.com>
2023-04-02 21:29:13 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker rm -f cui
 docker rmi -f chatgpt-ui
 docker build -t chatgpt-ui .
-docker run --name cui  -e OPENAI_API_HOST=http://10.0.0.4:8080 -e OPENAI_API_KEY=f9e8e3a55275e779ca5af319767ba106c4683fb2ca0dda1041da88305d668ad8 -d -p 3000:3000 chatgpt-ui
+docker run --name cui  -e OPENAI_API_HOST=http://192.168.0.154:8080 -e OPENAI_API_KEY=f9e8e3a55275e779ca5af319767ba106c4683fb2ca0dda1041da88305d668ad8 -d -p 3000:3000 chatgpt-ui
